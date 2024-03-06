5125bef103387f1afe89970fa92ab58f8b49be28
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-03-06 17:49:15 -0600

    Añadidas las vistas html y librerias

--- SolucionSistemaVenta/SistemaVenta.BLL/obj/Debug/net6.0/SistemaVenta.BLL.AssemblyInfo.cs
+++ SolucionSistemaVenta/SistemaVenta.BLL/obj/Debug/net6.0/SistemaVenta.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVenta.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5120a957a99d021c4baefc890135cd8164d38999")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2262aa92ee047a620f96ed9a1183f195e1087a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVenta.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVenta.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
