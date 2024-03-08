6166848e0c1bccfcb067a94cd4b3809f336896e4
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-03-08 15:38:49 -0600

    Primero minutos

--- SolucionSistemaVenta/SistemaVenta.AplicacionWeb/obj/Debug/net6.0/SistemaVenta.AplicacionWeb.AssemblyInfo.cs
+++ SolucionSistemaVenta/SistemaVenta.AplicacionWeb/obj/Debug/net6.0/SistemaVenta.AplicacionWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+799344c2c36d935009a4253856af178698881901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a93b85b3c41b32829f8965b4aa23c75da272534")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
